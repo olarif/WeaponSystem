1fe271067e9fa6dca7b9b50a9899b798f5019eb3
olarif <76481740+olarif@users.noreply.github.com>
2025-05-19 16:45:42 +0200

    vfx on hit component

---
+++ Assets/01_Scripts/Components/OnHit/VFXOnHit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXOnHit : OnHitComponent
+{
+    public List<GameObject> vfxEffects;
+    public float duration = 2f;
+    public override void Initialize(WeaponContext ctx)
+    {
+        base.Initialize(ctx);
+    }
+    
+    public override void OnHit(CollisionInfo info)
+    {
+        foreach (GameObject vfx in vfxEffects)
+        {
+            var fx = Instantiate(vfx, info.Point, Quaternion.identity);
+            Destroy(fx, duration);
+        }
+    }
+}

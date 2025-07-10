0a57f7b0981f3320ae81c740bf01023fe5f8222d
olarif <philip.nees1@gmail.com>
2025-07-10 23:34:47 +0200

    play sound action + fmod test project banks

---
+++ Assets/01_Scripts/Weapons/Actions/WeaponActions/PlaySoundAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+[System.Serializable]
+public class PlaySoundAction : IWeaponAction
+{
+    public EventReference soundEvent;
+    public bool isLooping = false;
+    
+    public void Execute(WeaponContext ctx, InputBindingData binding, ActionBindingData actionBinding)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(soundEvent, ctx.transform.position);
+    }
+}
